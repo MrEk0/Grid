83994a4062de534252403d11660b6a9bc46b6d04
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-05-08 15:04:48 +0300

    generate a game area

---
+++ Grid/Assets/GamePanel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GamePanel : MonoBehaviour
+{
+    [SerializeField] GameObject letterPrefab;
+
+    private int columsCount;
+    private int rowsCount;
+    private float letterWidth;
+    private float letterHeight;
+    private RectTransform rectTransform;
+    private List<Transform> letters;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        letterWidth = letterPrefab.GetComponent<RectTransform>().rect.width;
+        letterHeight = letterPrefab.GetComponent<RectTransform>().rect.height;
+
+        int maxColumns = Mathf.FloorToInt(rectTransform.rect.width / letterWidth);
+        int maxRows = Mathf.FloorToInt(rectTransform.rect.height / letterHeight);
+    }
+
+    public void GenerateLetters()
+    {
+        if (rowsCount == 0 || columsCount == 0)
+            return;
+
+        
+        letters = new List<Transform>();
+        Vector2 tempPos = new Vector2(-letterWidth * 0.5f, letterHeight * 0.5f);
+        Vector2 startPos = new Vector2(tempPos.x - letterWidth * (columsCount * 0.5f - 1), tempPos.y + letterHeight * (rowsCount * 0.5f - 1));
+
+        for (int i = 0; i < columsCount; i++)
+        {
+            for (int j = 0; j < rowsCount; j++)
+            {
+                Vector2 letterPos = new Vector2(startPos.x+letterWidth*i, startPos.y-letterHeight*j);
+                GameObject letter = Instantiate(letterPrefab, letterPos, Quaternion.identity);
+                letter.transform.SetParent(transform, false);
+                letters.Add(letter.transform);
+            }
+        }
+    }
+
+    public void ShuffleLetters()
+    {
+        List<Transform> takenPositions = new List<Transform>();
+        Transform newTransform;
+
+        foreach (var vector in letters)
+        { 
+            //do
+            //{
+                int posIndex = UnityEngine.Random.Range(0, letters.Count);
+                newTransform = letters[posIndex];
+            //}
+            //while (!takenPositions.Contains(newTransform));
+            Debug.Log(takenPositions.Contains(newTransform));
+            takenPositions.Add(newTransform);
+            vector.GetComponent<Letter>().ChangePosition(newTransform.position);
+        }
+    }
+
+    public void SetUpWidth(string width)
+    {
+        bool isDigit=width.All(char.IsDigit);
+        if(isDigit)
+        {
+            columsCount = int.Parse(width);
+        }
+        else
+        {
+            columsCount = 0;
+        }
+    }
+
+    public void SetUpHeight(string height)
+    {
+        bool isDigit = height.All(char.IsDigit);
+        if (isDigit)
+        {
+            rowsCount = int.Parse(height);
+        }
+        else
+        {
+            rowsCount = 0;
+        };
+    }
+}
---
+++ Grid/Assets/Letter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Letter : MonoBehaviour
+{
+    [SerializeField] float moveTime = 2f;
+    private void Start()
+    {
+        string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        int letterIndex = Random.Range(0, letters.Length);
+        GetComponent<TextMeshProUGUI>().text = letters[letterIndex];
+    }
+
+    public void ChangePosition(Vector3 newPos)
+    {
+        StartCoroutine(ChangePositionRoutine(newPos));
+    }
+
+    IEnumerator ChangePositionRoutine(Vector3 newPos)
+    {
+        float elapsedTime = 0f;
+        Vector2 startPos = transform.position;
+        for (float i = 0; i < moveTime; i += Time.deltaTime)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.position = Vector2.Lerp(startPos, newPos, elapsedTime);
+            yield return null;
+        }
+    }
+}
